ab2b7c425f12ff5977aaa53b1a27672f2f897f30
rebekkabb <breedis.rebekka@gmail.com>
2020-10-04 01:34:07 +0300

    Tutorial text appears on the first menu for 5 seconds

---
+++ Assets/Scripts/TutorialText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TutorialText : MonoBehaviour
+{
+
+    public TextMeshProUGUI tutorialText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowMessage("Press space to attack, use WASD to move. Watch out for th...", 5));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     IEnumerator ShowMessage (string message, float delay) {
+     tutorialText.text = message;
+     tutorialText.enabled = true;
+     yield return new WaitForSeconds(delay);
+     tutorialText.enabled = false;
+ }
+}
